eee8fa2c01fc01ffd1ccba279277f1ba115061f1
s-gabriele.tame@istitutopilati.it <s-gabriele.tame@istitutopilati.it>
2025-10-09 14:53:13 +0200

    init

--- Esercizio 16/main.c
+++ Esercizio 16/main.c
@@ -5,3 +5,30 @@ dove la A rappresenta la casa a minor consumo energetico la F quella a maggior c
 Chiedere all’utente i dati di tre case e poi dire qual è quella col minor costo al metro quadro e quella con la migliore classe energetica 
 (in caso di parità di livello di classe energetica si riporti quella più grande).
 */
+
+#include <stdio.h>
+
+struct Casa {
+    int mquadrati;
+    char classe[1];
+    double costo;
+
+};
+
+
+int main(){
+    struct Casa c[3];
+
+     for(int i=0;i<3;i++){
+        printf("Inserisci la dimensione della casa numero %d in metri quadrati: ",i+1); 
+        scanf("%d",&c[i].mquadrati);
+
+        printf("Inserisci la classe energetica della casa numero %d: ",i+1);
+        scanf("%s",&c[i].classe);
+
+        printf("Inserisci il costo della casa numero %d: ",i+1);
+        scanf("%lf",&c[i].costo);
+    }
+return 0;
+
+}
