04e5a300449e69ba64a69ba8f01d945299dff893
s-gabriele.tame@istitutopilati.it <s-gabriele.tame@istitutopilati.it>
2025-10-09 13:53:19 +0200

    init

--- Esercizio 13/main.c
+++ Esercizio 13/main.c
@@ -1,18 +1,43 @@
-/*Dato il seguente prototipo:
-void stampaArray(int *array, int dimensione);
-implementa una procedura che stampa tutti gli elementi dell'array*/
+/*Scrivere una funzione che, dati in input n è m allochi la memoria per una matrice nxm (n=righe e m=colonne)
+e ne restituisca il contenuto in un puntatore a puntatore.*/
 
 #include <stdio.h>
+#include <stdlib.h>
 
-void stampaArray(int *array, int dimensione){
-    for(int i=0;i<dimensione;i++){
-        printf("%d\n",array[i]);
-    }
+int **matrice(int n, int m){
+    int **ptr = (int**) malloc(sizeof(int*)*n); 
+    
+    for(int i=0;i<n;i++)
+        ptr[i] = (int*) malloc(m*sizeof(int));
+
+    return ptr;
 }
 
 int main(){
-    int vet[] = {1,2,3,4};
-    stampaArray(vet,4);
+    
+    int x, y;
 
+    printf("Inserisci il numero di righe: ");
+    scanf("%d", &x);
+    printf("Inserisci il numero di colonne: ");
+    scanf("%d", &y);
+
+    int **mat = matrice(x,y);
+
+    printf("Inserisci i valori della matrice:\n");
+    for(int i=0; i<x;i++){
+        for(int j=0;j<y;j++){
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    printf("\nLa matrice inserita e':\n");
+    for(int i=0;i<x;i++){
+        for(int j=0;j<y;j++){
+            printf("[%d] ", mat[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
