14a42411d943a04214625fcf76ba8572fec93c1a
s-gabriele.tame@istitutopilati.it <s-gabriele.tame@istitutopilati.it>
2025-11-10 12:05:21 +0100

    init

---
+++ C++/Esercizio 6/main.cpp
@@ -0,0 +1,11 @@
+/*
+Si crei una classe scarpe con gli strectch (linguette adesive col feltro); 
+ogni paio di scarpe è caratterizzato dalla dimensione 
+(data da un numero intero) e ha tre stretch sulla scarpa destra e tre sulla scarpa sinistra.
+Creare due paia di scarpe con gli stretch e per 10 volte estrarre 
+casualmente su quale paio di scarpe cambiare da allacciato a slacciato uno 
+degli stretch (scelto casualmente) di una delle due scarpe scelta sempre casualmente.
+Dopo aver fatto aprire e chiudere per le 10 volte gli stretch, 
+stampare la situazione finale delle scarpe; si consideri che 
+inizialmente tutti gli stretch sono slacciati.
+*/
