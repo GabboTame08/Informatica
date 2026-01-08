b37dc398b650c978d0921fd1c880c1844cbbea64
s-gabriele.tame@istitutopilati.it <s-gabriele.tame@istitutopilati.it>
2026-01-08 14:46:13 +0100

    init

--- C++/Stack/Stack.cpp
+++ C++/Stack/Stack.cpp
@@ -1,2 +1,37 @@
 #include "Stack.h"
+#define MAX_STACK_SIZE
 
+void push(int n){
+    if(t == MAX_STACK_SIZE-1){
+        cout << "errore, stack pieno.";
+    }
+    else
+        vet[t++] == n;
+}
+
+int pop(){
+    if(isEmpty()){
+        cout << "errore, stack vuoto.";
+    }
+    else
+        return vet[--t];
+}
+
+int top(){
+    return vet[MAX_STACK_SIZE-1];
+}
+bool isEmpty(){
+    if(vet[0] == "")
+        return true;
+    else   
+        return false;
+}
+
+int size(){
+    int counter;
+    for(int i=0; i<MAX_STACK_SIZE;i++){
+        if(vet[i] == ""){ 
+            return i;
+        }
+    }
+}
